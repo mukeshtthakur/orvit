cc6d49a3bdf98d8c668c60fa53cecf695d2ff0f5
mthakurgit <mukesh6773@gmail.com>
2019-11-19 09:32:51 +0530

    add login signup authentication

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const _ = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");// install passport passport-local passport-local-mongoose express-session
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+//variable
 
+var title = "";
+var content = "";
+
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+//starting a session
+app.use(session({
+  secret:process.env.SECRET,
+  resave:false,
+  saveUninitialized: false
+}));
+
+
+//use passport package
+app.use(passport.initialize());
+//use passport package to start the session
+app.use(passport.session());
 
 
 // connect mongoose
@@ -23,30 +41,117 @@ mongoose.connect("mongodb://localhost:27017/blogDB", {useCreateIndex: true, useN
 
 
 //create schema
-const postSchema = {
+const postSchema =new mongoose.Schema({
   title: String,
-  content: String
-};
+  content: String,
+
+});
 // mongoose model
 const Post = mongoose.model("Post", postSchema);
 
 
-//home route
+
+
+const userSchema = new mongoose.Schema( {
+  email: String,
+  password: String,
+  googleId: String,
+});
+
+//encryption
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+//creating a model
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/home",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+
 app.get("/", function(req, res){
+  res.render("root");
+});
 
-  Post.find({},function(err, posts){
-    res.render("home",{
-      startingContent:homeStartingContent,
-      posts:posts,
+//home route
+app.get("/home", function(req, res){
+
+  if(req.isAuthenticated()){
+    Post.find({},function(err, posts){
+      res.render("home",{
+        posts:posts,
+       });
      });
-   });
+  }else{
+    res.redirect("/login");
+  }
+
+
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get("/auth/google/home",
+passport.authenticate('google', { failureRedirect: '/login' }),
+function(req, res) {
+  // Successful authentication, redirect to secrets.
+  res.redirect('/home');
+
+});
+
+//login route
+app.get("/login", function(req, res){
+  req.logout();
+  res.render("login");
 });
 
 
-// /about route
-app.get("/about", function(req, res){
-  res.render("about",{aboutContent:aboutContent});
+// signup route
+app.get("/signup", function(req, res){
+  req.logout();
+  res.render("signup");
 });
+
+//log out
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+
 
 
 // /contact me route
@@ -58,7 +163,11 @@ app.get("/contact", function(req, res){
 // /Compose route
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+  if(req.isAuthenticated()){
+    res.render("compose");
+  }else{
+    res.redirect("/login");
+  }
 });
 
 
@@ -77,6 +186,41 @@ const requestedPostId = req.params.postId;
 });
 
 
+app.post("/signup", function(req, res){
+  req.logout();
+  User.register({username:req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/signup");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+  });
+
+});
+
+app.post("/login", function(req, res){
+  req.logout();
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+  });
+
+});
+
+
 app.post("/compose", function(req, res){
    const post = new Post({
      title: req.body.postTitle,
@@ -84,7 +228,7 @@ app.post("/compose", function(req, res){
    });
    post.save(function(err){
      if(!err){
-       res.redirect("/");
+       res.redirect("/home");
      }
    });
 });
